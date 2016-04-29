41a2173bab109d852841f8239b140ce3ccd8bff1
jclin <jclin@articulate.com>
2016-04-28 17:44:54 -0700

    Added script to enable PS Remoting

---
+++ Enable-PSRemoting.ps1
@@ -0,0 +1,13 @@
+try
+{
+    Write-Debug "Setting all network connection profiles to Private"
+    Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private -ErrorAction Stop
+    
+    Write-Debug "Enabling Powershell Remoting"
+    Enable-PSRemoting -Force -ErrorAction Stop
+}
+catch
+{
+    Write-Debug $
+    throw
+}
