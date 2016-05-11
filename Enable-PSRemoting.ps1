2b1ec6d9c14a69096597688224e850fd71cb0b96
jclin <jclin@articulate.com>
2016-05-11 13:43:31 -0700

    Set up custom PSSessionConfig during deployment
    This custom session configuration will allow large files to be sent/received over a PS remoting session.

--- Enable-PSRemoting.ps1
+++ Enable-PSRemoting.ps1
@@ -1,10 +1,16 @@
+$ErrorActionPreference = "Stop"
+
 try
 {
     Write-Output "Setting all network connection profiles to Private"
-    Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private -ErrorAction Stop
+    Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
 
     Write-Output "Enabling Powershell Remoting"
-    Enable-PSRemoting -Force -ErrorAction Stop
+    Enable-PSRemoting -Force
+
+    Write-Output "Setting up a custom PSSessionConfig for sending large files over a remote session"
+    Register-PSSessionConfiguration -Name "Articulate.PSSessionConfig" -Force
+    Set-PSSessionConfiguration -Name "Articulate.PSSessionConfig" -MaximumReceivedDataSizePerCommandMB 500 -MaximumReceivedObjectSizeMB 500 -Force
 }
 catch
 {
